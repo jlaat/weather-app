192105ca36731620c401caea7dc21e4854df96d4
Joni Laatikainen <jonilaatikaine@gmail.com>
2022-07-24 13:50:16 +0300

    Method for processing needed weather data done

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -1,20 +1,22 @@
 // Fetch weather data from given location
 const fetchData = (location) => {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=a8670a42bf4a7d51171b9efe3c89af7b`)
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=a8670a42bf4a7d51171b9efe3c89af7b&units=metric`
+  )
     .then((response) => {
-        return response.json();
+      return response.json();
     })
     .then((data) => {
-        processData(data);
-    })
-}
+      processData(data);
+    });
+};
 
 // Process weather data needed for website
 const processData = (data) => {
-    let weatherData = {
-        location: data.name,
-        temperature: data.main.temp,
-        
-    }
-    
-} 
+  let weatherData = {
+    location: data.name,
+    temperature: data.main.temp,
+    icon: data.weather[0].icon,
+  };
+  console.log(weatherData);
+};
