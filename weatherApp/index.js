2366e421d4ce9644b6b5d6f1213a9ada0c7d4b5e
Joni Laatikainen <jonilaatikaine@gmail.com>
2022-08-16 20:53:52 +0300

    Modified data fetching functions

--- weatherApp/index.js
+++ weatherApp/index.js
@@ -1,14 +1,11 @@
 // Fetch weather data from given location
-const fetchData = (location) => {
-  fetch(
+const fetchData = async (location) => {
+  const response = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=a8670a42bf4a7d51171b9efe3c89af7b&units=metric`
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      processData(data);
-    });
+  );
+  const responseJson = await response.json();
+
+  processData(responseJson);
 };
 
 // Process weather data needed for website
@@ -18,7 +15,7 @@ const processData = (data) => {
     temperature: data.main.temp,
     icon: data.weather[0].icon,
     feelsLike: data.main.feels_like,
-    windspeed: data.wind.speed
+    windspeed: data.wind.speed,
   };
   refreshWeatherDisplay(weatherData);
 };
@@ -30,14 +27,12 @@ const searchClicked = () => {
   fetchData(location.value);
 };
 
-
 const refreshWeatherDisplay = (weatherObject) => {
   changeIcon(weatherObject.icon);
   changeLocation(weatherObject.location);
   changeTemperature(weatherObject.temperature);
   changeFeelsLike(weatherObject.feelsLike);
   changeWind(weatherObject.windspeed);
-
 };
 
 const changeIcon = (iconName) => {
@@ -46,7 +41,7 @@ const changeIcon = (iconName) => {
   ).src = `http://openweathermap.org/img/wn/${iconName}@2x.png`);
 };
 const changeTemperature = (newTemperature) => {
-  let divTemperature = (document.querySelector(".temperature").textContent =` 
+  let divTemperature = (document.querySelector(".temperature").textContent = ` 
     ${newTemperature} °C`);
 };
 const changeLocation = (newLocation) => {
@@ -54,11 +49,15 @@ const changeLocation = (newLocation) => {
     newLocation);
 };
 const changeFeelsLike = (newTemperature) => {
-  let divFeelsLike = document.querySelector(".feelsLike").textContent = `Feels like: ${newTemperature} °C`;
-}
+  let divFeelsLike = (document.querySelector(
+    ".feelsLike"
+  ).textContent = `Feels like: ${newTemperature} °C`);
+};
 const changeWind = (newWind) => {
-  let divWind = document.querySelector(".wind").textContent = `Wind: ${newWind} m/s`
-}
+  let divWind = (document.querySelector(
+    ".wind"
+  ).textContent = `Wind: ${newWind} m/s`);
+};
 
 // Display Joensuu's weather on startup
 fetchData("Joensuu");
